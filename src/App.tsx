88175e8ec7226b6ca7f024ed9628985c863d139d
Barry Bach <bbach7139@gmail.com>
2024-05-24 14:26:55 -0500

    Finish base layout; remove SVG files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 
 function App() {
   const [step, setStep] = useState(0)
 
   return (
-    <div className={`bg-gray-800 text-white h-dvh`}>
+    <div className={`bg-gray-800 text-white h-dvh flex flex-col`}>
       <Header />
+      
+      <Footer />
     </div>
   )
 }
